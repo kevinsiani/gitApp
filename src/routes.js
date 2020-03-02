72ab39ce3aadd0b72495777742c83e81ae9f98dc
Kevin Siani <sianikevin@gmail.com>
2020-03-02 14:00:21 -0300

    Fix import

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
-import Repository from ',/pages/Repository';
+import Repository from './pages/Repository';
 
 export default function Routes() {
   return (
