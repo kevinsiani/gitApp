19a9e5823a114c11700c8ebadd8d7810c0941b32
Kevin Siani <sianikevin@gmail.com>
2020-03-03 12:43:45 -0300

    Add css main and axios to fetch data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  hnadleInputchange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
   };
 
   render() {
+    const { newRepo, loading } = this.state;
+
     return (
       <Container>
         <h1>
@@ -17,11 +46,20 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form>
-          <input type="text" placeholder="Adicionar repositório" />
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.hnadleInputchange}
+          />
 
-          <SubmitButton>
-            <FaPlus color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
           </SubmitButton>
         </Form>
       </Container>
